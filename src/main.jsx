b434e98c134587040b1c0dbe85a32ad4d8cfd61b
MaheryAndrih1 <maheryandrianaivo1@gmail.com>
2025-04-01 02:13:50 +0300

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './style.css'
+import './style.css' // This import now works with the file we created
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
